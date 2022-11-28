108bdcffcd751f2ce82bf72318ab63733444e2bc
Otto Sierra <manomarrano@gmail.com>
2022-11-27 19:49:32 -0800

    Vue Ver

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,27 +1,83 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      itemInput: '',
+      editInput: '',
+      currentEditingItem: 0,
+      editFormStyles: {
+        display: 'none',
+        opacity: "0"
+      },
+      listItems: [
+        'Apple',
+        'Google',
+        'Microsoft'
+      ]
+    }
+  },
+  methods: {
+    addItem() {
+      if (this.itemInput == '') return;
+
+      this.listItems.push(this.itemInput);
+      this.itemInput = '';
+    },
+    deleteItem(index) {
+      this.listItems.splice(index, 1);
+    },
+    editItem(index) {
+      requestAnimationFrame(() => {
+        this.editFormStyles.display = "flex";
+        requestAnimationFrame(() => {
+          this.editFormStyles.opacity = "1";
+        });
+      });
+      
+
+      this.editInput = this.listItems[index];
+      this.currentEditingItem = index;
+    },
+    updateItem() {
+      this.listItems[this.currentEditingItem] = this.editInput;
+      this.closeEdit();
+    },
+    closeEdit() {
+      this.editFormStyles.opacity = "0";
+      setTimeout(() => {
+          this.editFormStyles.display = "none";
+          this.editInput = '';
+      }, 200);
+    }
+  }
+}
 </script>
 
 <template>
   <main class="container">
       <h1>ListApp</h1>
       <p>Add new items to the list.</p>
-      <form id="form">
+      <form id="form" @submit.prevent="addItem">
           <label for="input">New Item</label>
-          <input type="text" id="input" autocomplete="off"><button onclick="addItem()" type="submit">Submit</button>
+          <input type="text" id="input" autocomplete="off" v-model="itemInput">
+          <button type="submit">Submit</button>
       </form>
       
       <ul id="list">
-          <!-- ITEMS -->
+        <!-- ITEMS -->
+        <li v-for="(item, index) in listItems">
+          {{ item }}
+          <button class="button-clear button-small button-green" @click="editItem(index)">Edit</button>
+          <button class="button-clear button-small button-red" @click="deleteItem(index)">Delete</button>
+        </li>
       </ul>
   </main>
-  <form id="edit-form">
+  <form id="edit-form" v-bind:style="{ display: editFormStyles.display, opacity: editFormStyles.opacity }">
       <div id="edit-form-content">
           <label for="edit-input">Edit Item</label>
-          <input type="text" id="edit-input" autocomplete="off">
-          <button onclick="closeEdit()" type="button">Cancel</button>
-          <button onclick="updateItem()" type="submit">Save</button>
+          <input type="text" id="edit-input" autocomplete="off"  v-model="editInput">
+          <button @click="closeEdit()" type="button">Cancel</button>
+          <button @click.prevent="updateItem()" type="submit">Save</button>
       </div>
   </form>
 </template>
